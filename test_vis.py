66ac194cfdf99acae6b9e0ec5c9255f44277a787
pdmurray <peyton.murray@tuni.fi>
2019-04-26 17:06:09 +0300

    troubleshooting velocity

--- test_vis.py
+++ test_vis.py
@@ -10,11 +10,17 @@ import mx3tools.statutil as statutil
 import mx3tools.datautil as datautil
 import mx3tools.plotutil as plotutil
 
+
+def mz_to_pos_in_window(mz):
+    return (1 - (1 - mz)/2)*1024*2e-9
+
+
 size = 18
 matplotlib.rc('axes', labelsize=size)
 
 # data_lowdmi = datautil.SimRun('/home/pdmurray/Desktop/Workspace/dmidw/barkhausen/D_range/2019-04-17')
 data_highdmi = datautil.SimRun('/home/pdmurray/Desktop/Workspace/dmidw/barkhausen/D_0.6e-3/2019-04-16')
+# data_highdmi = datautil.SimData(script='', data_dir='/home/pdmurray/Desktop/Workspace/dmidw/barkhausen_0.out')
 
 # times, signals = data_highdmi.events_by_duration(duration=0.5e-9, tol=0.25e-10)
 # tbin, sbin = statutil.bin_avg(times, signals, nbins=None)
@@ -22,7 +28,7 @@ data_highdmi = datautil.SimRun('/home/pdmurray/Desktop/Workspace/dmidw/barkhause
 
 # data_lowdmi = datautil.SimRun('/home/pdmurray/Desktop/Workspace/dmidw/barkhausen/D_range/2019-04-17')
 
-fig, ax = plt.subplots(nrows=3, sharex=True, figsize=(15, 8))
+fig, ax = plt.subplots(nrows=4, sharex=True, figsize=(15, 8))
 # plotutil.burst(ax, data_lowdmi[0], 'viridis')
 
 # for _t, _s in zip(times, signals):
@@ -37,8 +43,10 @@ fig, ax = plt.subplots(nrows=3, sharex=True, figsize=(15, 8))
 # plotutil.plot_dt(ax, data_highdmi[0])
 
 ax[0].plot(data_highdmi[0].t(), data_highdmi[0].vdw(), '-k')
-ax[1].plot(data_highdmi[0].t()[:-1], np.diff(data_highdmi[0].dwpos()), '-r')
-ax[2].plot(data_highdmi[0].t()[:-1], np.diff(data_highdmi[0].t()), '-b')
+ax[1].plot(data_highdmi[0].t(), data_highdmi[0].dwpos(), '-r')
+ax[2].plot(data_highdmi[0].t(), data_highdmi[0].shift(), '-b')
+ax[3].plot(data_highdmi[0].t(), mz_to_pos_in_window(data_highdmi[0].table['mz ()']), '-g')
 
+plt.tight_layout()
 
 plt.show()
