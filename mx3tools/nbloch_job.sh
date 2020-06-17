0f367c0c1c0ebb1887bf105555bd2a1edb9fc654
Peyton Murray <peytonmurray@gmail.com>
2020-06-16 17:08:34 -0700

    Remove old unused c++ code

--- mx3tools/nbloch_job.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/home/pdmurray/Desktop/Workspace/mx3tools/mx3tools/dwread -i /mnt/Data/dmidw/D_0.0e-3/2019-05-26/ -o /mnt/Data/dmidw/D_0.0e-3/2019-05-26/nbloch.txt
-/home/pdmurray/Desktop/Workspace/mx3tools/mx3tools/dwread -i /mnt/Data/dmidw/D_0.3e-3/2019-05-18/ -o /mnt/Data/dmidw/D_0.3e-3/2019-05-18/nbloch.txt
-/home/pdmurray/Desktop/Workspace/mx3tools/mx3tools/dwread -i /mnt/Data/dmidw/D_0.6e-3/2019-04-18/ -o /mnt/Data/dmidw/D_0.6e-3/2019-04-18/nbloch.txt
